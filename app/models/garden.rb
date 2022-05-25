0b983cb05ec3950f9beca74ea529ddd1d21bd125
Andre Menezes <djdedemenezes@gmail.com>
2022-05-25 18:26:19 -0300

    gardens and plats done

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,4 +1,6 @@
 class Garden < ApplicationRecord
+  has_many :plants, dependent: :destroy # => what would happen with the "orphan" plants
+
   validates :name, :banner_url, presence: true
   validates :name, length: { minimum: 3 }
   validates :banner_url, format: { with: /\Ahttp.+\.(jpg|png)/ }
